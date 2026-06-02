705d2f4700c8444a53b1787a82624829d7013fac
Dani <danieltraykov@gmail.com>
2026-06-02 16:51:29 +0300

    feat: implement box blur algorithm with edge case handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,17 +11,40 @@ public class Main {
             BufferedImage image = ImageIO.read(inputFile);
             int width = image.getWidth();
             int height = image.getHeight();
-            System.out.println("Ширина: " + width + " px");
-            System.out.println("Височина: " + height + " px");
-            int pixelRGB = image.getRGB(0, 0);
-            Color color = new Color(pixelRGB);
+            BufferedImage outputImage = new BufferedImage(width, height, image.getType());
 
-            System.out.println("Пиксел (0,0) цветове:");
-            System.out.println("R: " + color.getRed());
-            System.out.println("G: " + color.getGreen());
-            System.out.println("B: " + color.getBlue());
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int sumR = 0, sumG = 0, sumB = 0;
+                    int validPixels = 0;
+
+                    for (int ky = -1; ky <= 1; ky++) {
+                        for (int kx = -1; kx <= 1; kx++) {
+                            int neighborX = x + kx;
+                            int neighborY = y + ky;
+                            if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height) {
+                                int pixelRGB = image.getRGB(neighborX, neighborY);
+                                Color color = new Color(pixelRGB);
+                                sumR += color.getRed();
+                                sumG += color.getGreen();
+                                sumB += color.getBlue();
+                                validPixels++;
+                            }
+                        }
+                    }
+
+                    int avgR = sumR / validPixels;
+                    int avgG = sumG / validPixels;
+                    int avgB = sumB / validPixels;
+                    Color blurredColor = new Color(avgR, avgG, avgB);
+                    outputImage.setRGB(x, y, blurredColor.getRGB());
+                }
+            }
+
+            File outputFile = new File("blurred-image.jpg");
+            ImageIO.write(outputImage, "jpg", outputFile);
         } catch (IOException e) {
-            System.out.println("Грешка при зареждане на изображението: " + e.getMessage());
+            System.out.println("Image load error: " + e.getMessage());
         }
     }
 }
